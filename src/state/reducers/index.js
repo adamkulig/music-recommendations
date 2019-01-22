3214865000f4f7f08ca5c361c96704c66e16f796
Adam Kulig <Adam.Kulig@rossmann.com.pl>
2019-01-22 16:45:55 +0100

    toastr

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import { firestoreReducer } from 'redux-firestore';
 import { firebaseReducer } from 'react-redux-firebase';
 import { reducer as formReducer } from 'redux-form';
+import { reducer as toastrReducer } from 'react-redux-toastr'
 import authReducer from './auth.reducers';
 import votingReducer from './voting.reducers';
 
@@ -10,7 +11,8 @@ const rootReducer = combineReducers ({
   voting: votingReducer,
   firestore: firestoreReducer,
   firebase: firebaseReducer,
-  form: formReducer
+  form: formReducer,
+  toastr: toastrReducer
 });
 
 export default rootReducer;
