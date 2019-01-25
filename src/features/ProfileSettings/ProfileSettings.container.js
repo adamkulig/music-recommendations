1c5c9e4ca357a0cba0fea115c84d162e01544bba
Adam Kulig <Adam.Kulig@rossmann.com.pl>
2019-01-25 17:05:58 +0100

    route-security

--- src/features/ProfileSettings/ProfileSettings.container.js
+++ src/features/ProfileSettings/ProfileSettings.container.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { Field } from 'redux-form';
+import { connect } from 'react-redux';
 import ProfileSettings from './ProfileSettings.component';
+import { Redirect } from 'react-router-dom';
+import routes from '../../variables/routes';
 
 class ProfileSettingsContainer extends Component {
   render() {
+    const { firebaseAuth } = this.props;
+    const { isEmpty } = firebaseAuth;
+    if (isEmpty) {
+      return <Redirect to={routes.Main} />
+    }
     return (
       <ProfileSettings />
     );
   }
 }
 
-export default (ProfileSettingsContainer);
+const mapStateToProps = state => ({
+  firebaseAuth: state.firebase.auth,
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(ProfileSettingsContainer);
+
